4d61d155bd43b336b822675c4b3cd80e1189c29d
BURAK YUCAL <burakyucal@gmail.com>
2025-05-29 18:13:38 +0200

    u

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def check_google():
     try:
-        response = requests.get('healthchecks-aag9emhhg3efadcy.germanywestcentral-01.azurewebsites.net', timeout=5)
+        response = requests.get('https://healthchecks-aag9emhhg3efadcy.germanywestcentral-01.azurewebsites.net', timeout=5)
         if response.status_code == 200:
-            return 'healthchecks-aag9emhhg3efadcy.germanywestcentral-01.azurewebsites.net is up!', 200
+            return 'Google is up!', 200
         else:
-            return f'healthchecks-aag9emhhg3efadcy.germanywestcentral-01.azurewebsites.net returned status code {response.status_code}', 503
+            return f'Google returned status code {response.status_code}', 503
     except requests.RequestException as e:
-        return f'healthchecks-aag9emhhg3efadcy.germanywestcentral-01.azurewebsites.net is not reachable: {e}', 503
+        return f'Google is not reachable: {e}', 503
 
 if __name__ == '__main__':
     app.run()

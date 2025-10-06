85b702b080c2ae60a9bd65402a7f64a88925f131
BURAK YUCAL <burakyucal@gmail.com>
2025-10-06 21:48:34 +0200

    Fix indentation in get_service_status_from_panos function for consistency

--- app/crud/crud.py
+++ app/crud/crud.py
@@ -2,10 +2,11 @@
 from typing import Optional
 from app.modules.schemas import ServiceStatus
 
+
 def get_service_status_from_panos() -> Optional[ServiceStatus]:
-            return ServiceStatus(
-                service_name="PAN-OS",
-                status="OK",
-                detail="Service is running."
-            )
+    return ServiceStatus(
+        service_name="PAN-OS",
+        status="OK",
+        detail="Service is running."
+    )
         

7de2a1be67fbde9fa3e2ad683bace02f708e9ee2
BURAK YUCAL <burakyucal@gmail.com>
2025-11-23 23:04:37 +0100

    Add logging for fetched static restaurant data in get_static_data function

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from schemas.dataschemas import DataPayload
 from v1 import data
+import logging
 router = APIRouter(prefix="/v1", tags=["Data API"])
 
 @router.post("/restaurant", response_model=DataPayload)
@@ -12,4 +13,5 @@ async def echo_data(payload: DataPayload):
 @router.get("/restaurant", response_model=DataPayload)
 async def get_static_data():
     static_data = data.fetch_restaurant_data()
+    logging.info(f"Fetched static restaurant data:{static_data}")
     return static_data

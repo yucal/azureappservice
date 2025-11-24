83b80422bd061551766d6123fe74ebebafb14263
BURAK YUCAL <burakyucal@gmail.com>
2025-11-24 10:30:13 +0100

    Enhance logging in fetch_restaurant_data function and update SAS URL

--- app/api/v1/data.py
+++ app/api/v1/data.py
@@ -1,13 +1,14 @@
 from azure.storage.blob import BlobClient
-import json
+import json,logging
 def fetch_restaurant_data():
-    sas_url = "https://beerapistorage.blob.core.windows.net/beerapi-container/data.json?sp=r&st=2025-11-23T21:22:18Z&se=2025-11-24T05:37:18Z&spr=https&sv=2024-11-04&sr=b&sig=ccNl83fow3AeBOYhki1c1TtM85Esv%2FBAhjGrpbigN2k%3D"
-
+    sas_url = "https://beerapistorage.blob.core.windows.net/beerapi-container/data.json?sp=r&st=2025-11-24T09:12:12Z&se=2025-11-24T17:27:12Z&spr=https&sv=2024-11-04&sr=b&sig=bXJ5sVrvsZIcMYjYBf2EvSZ%2F9%2FhLbf2IDurXZt3yqsg%3D"
+    logging.info("Starting to download blob from SAS URL.")
     # Create a BlobClient directly from the SAS URL
     blob = BlobClient.from_blob_url(sas_url)
 
     # Download file bytes
     file_bytes = blob.download_blob().readall()
-
+    logging.info("Blob downloaded successfully.")
     json_data = json.loads(file_bytes)
+    
     return (json_data.get("restaurants"))
